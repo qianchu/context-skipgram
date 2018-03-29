0195073c9fa9d28a1cf45a70b2f6d9bd1d616abd
root <hey_flora@126.com>
2018-03-29 22:31:53 +0000

    delete conda

--- start.sh
+++ start.sh
@@ -4,20 +4,20 @@ apt-get update
 apt-get install vim
 
 #install conda
-file='/home/Anaconda3-5.0.1-Linux-x86_64.sh'
-if echo $PATH | grep -q anaconda3
-then
-	echo 'conda installed'
-else
-    echo "$0: File '${file}' not found."
-	cd /home/
-	curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-	sha256sum Anaconda3-5.0.1-Linux-x86_64.sh
-	bash ${file}
+# file='/home/Anaconda3-5.0.1-Linux-x86_64.sh'
+# if echo $PATH | grep -q anaconda3
+# then
+# 	echo 'conda installed'
+# else
+#     echo "$0: File '${file}' not found."
+# 	cd /home/
+# 	curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+# 	sha256sum Anaconda3-5.0.1-Linux-x86_64.sh
+# 	bash ${file}
 	
-	urce ~/.bashrc
+# 	urce ~/.bashrc
 
-fi
+# fi
 
 
 # run jupyter
